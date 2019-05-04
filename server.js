d47a826e8b0096acd2148cc4685d9b1ef1bf11eb
M. Y. Katz <mykatz01@gmail.com>
2019-05-04 16:12:28 -0700

    Misc frontend

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 
+require('dotenv').config()
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -10,7 +12,14 @@ app.use('/assets', express.static("assets"))
 // === routes ===
 
 app.get('/', function(req, res){
-    res.render('home')
+    res.render('home', {
+        school: [
+            {name: "Stanford", rank: 1, elo: 1700},
+            {name: "Harvard", rank: 2, elo: 1500},
+            {name: "MIT", rank: 3, elo: 1400},
+            {name: "Carnegie Mellon", rank: 4, elo: 1390}
+        ]
+    })
 });
 
 app.listen(80, () => console.log('Server started on port 80'));
